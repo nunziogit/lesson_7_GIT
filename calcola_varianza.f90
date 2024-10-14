9e469464c28e3cf37bc6b764547254cae87fd55f
nunziogit <annunziato.siviglia@unitn.it>
2024-10-14 15:05:35 +0200

    I have changed nothin almost

--- calcola_varianza.f90
+++ calcola_varianza.f90
@@ -4,7 +4,9 @@ SUBROUTINE calcola_varianza(array, media, n, varianza)
    REAL, INTENT(IN), DIMENSION(n) :: array
    REAL, INTENT(IN) :: media
    REAL, INTENT(OUT) :: varianza
+   REAL :: varianza2
    ! Calculate the standard deviation
    varianza = SQRT(sum((array - media)**2) / REAL(n))
+   varianza2 = SQRT(sum((array - media)**3) / REAL(n))
    RETURN
 END SUBROUTINE calcola_varianza
